96102f18144dec5a3fa8debac5f389becafd6ec8
repl.it user <replituser@example.com>
2020-10-21 18:10:26 +0000

    new try

--- heap.c
+++ heap.c
@@ -24,7 +24,23 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
+  if(pq->size+1>pq->capac){
+        //printf("se expande de %i a ", pq->capac);
+        pq->capac=(pq->capac)*2+1;
+        //printf("%i * %lu", pq->capac, sizeof(heapElem));
+        pq->heapArray=realloc(pq->heapArray, (pq->capac)*sizeof(heapElem));
+    }
 
+    /*Flotación*/
+    int now = pq->size;
+    while(now>0 && pq->heapArray[(now-1)/2].priority < priority)
+        {
+                pq->heapArray[now] = pq->heapArray[(now-1)/2];
+                now = (now -1)/2;
+        }
+    pq->heapArray[now].priority = priority;
+    pq->heapArray[now].data = data;
+    pq->size++;
 }
 
 
